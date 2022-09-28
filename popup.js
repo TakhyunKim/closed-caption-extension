d29665c2921f3069225b140ee4762a8db1634fcc
Takhyun Kim <kimkih1218@lunit.io>
2022-09-29 00:22:16 +0900

    feat: initial setting

---
+++ popup.js
@@ -0,0 +1,7 @@
+const translationElement = document.getElementById("translate");
+
+translationElement.addEventListener("click", handleTranslationClick);
+
+function handleTranslationClick() {
+  alert("first alert");
+}
