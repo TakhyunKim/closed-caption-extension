2f64f5ed2e631367115e30ff243bdfd3c17f553b
Takhyun Kim <kimkih1218@gmail.com>
2022-09-30 01:11:13 +0900

    feat: apply new dom element creating and apply style to new dom element

--- popup.js
+++ popup.js
@@ -1,12 +1,29 @@
 const translationElement = document.getElementById("translate");
 
-const getClosedCaptionTextInfo = () => {
-  const closedCaptionElement = document.querySelector(
-    ".vjs-text-track-display"
-  );
+const getClosedCaptionInfo = () => {
+  const closedCaptionElement = document.querySelector(".vjs-text-track-cue");
+  const closedCaptionWrapperElement = closedCaptionElement.parentElement;
 
+  const insetStyle = closedCaptionElement.style.inset;
   const textContent = closedCaptionElement.textContent;
-  const position = closedCaptionElement.getBoundingClientRect();
+
+  const newClosedCaptionWrapperElement = document.createElement("div");
+
+  newClosedCaptionWrapperElement.style.textAlign = "center";
+  newClosedCaptionWrapperElement.style.position = "absolute";
+  newClosedCaptionWrapperElement.style.inset = `${
+    Number(insetStyle.split(" ")[0].replace("px", "")) + 50
+  }px`;
+  newClosedCaptionWrapperElement.classList.add("vjs-text-track-cue");
+
+  const newClosedCaptionElement = document.createElement("div");
+
+  newClosedCaptionElement.textContent = "안녕";
+  newClosedCaptionElement.style.color = "rgb(255, 255, 255)";
+  newClosedCaptionElement.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+
+  newClosedCaptionWrapperElement.appendChild(newClosedCaptionElement);
+  closedCaptionWrapperElement.appendChild(newClosedCaptionWrapperElement);
 };
 
 translationElement.addEventListener("click", async () => {
@@ -14,6 +31,6 @@ translationElement.addEventListener("click", async () => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: getClosedCaptionTextInfo,
+    function: getClosedCaptionInfo,
   });
 });
