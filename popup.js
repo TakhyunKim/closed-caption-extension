d0dd6d7ce86345dbfa50d62f838ab613fa270360
Takhyun Kim <kimkih1218@gmail.com>
2022-09-29 23:28:14 +0900

    feat: add get target dom text content, position

--- popup.js
+++ popup.js
@@ -1,7 +1,19 @@
 const translationElement = document.getElementById("translate");
 
-translationElement.addEventListener("click", handleTranslationClick);
+const getClosedCaptionTextInfo = () => {
+  const closedCaptionElement = document.querySelector(
+    ".vjs-text-track-display"
+  );
 
-function handleTranslationClick() {
-  alert("first alert");
-}
+  const textContent = closedCaptionElement.textContent;
+  const position = closedCaptionElement.getBoundingClientRect();
+};
+
+translationElement.addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: getClosedCaptionTextInfo,
+  });
+});
