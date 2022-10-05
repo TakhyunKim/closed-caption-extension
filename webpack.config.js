2b81bf4fba6fd5cb8c8358ff229394abcba59df4
Takhyun Kim <kimkih1218@gmail.com>
2022-10-05 17:00:56 +0900

    chore: apply typescript compile using webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: process.env.ENV || "development",
+  entry: {
+    popup: path.resolve(__dirname, "src", "popup.ts"),
+    background: path.resolve(__dirname, "src", "background.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: ".", to: ".", context: "public" }],
+    }),
+    new HtmlWebpackPlugin({
+      title: "popup",
+      chunks: ["popup"],
+      filename: "popup.html",
+      template: "./src/popup.html",
+    }),
+  ],
+};
