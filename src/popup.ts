57cb6c23f7ee24bc35fbedc00ee64ff9f1b6ebc6
Takhyun Kim <kimkih1218@gmail.com>
2022-10-30 17:46:26 +0900

    feat: save isChecked value to chrome storage

--- src/popup.ts
+++ src/popup.ts
@@ -4,16 +4,41 @@ const translationElement = document.getElementById(
   "translate"
 ) as HTMLInputElement;
 
-translationElement.addEventListener("click", async () => {
+const sendToMessageIsActiveTranslation = async (
+  isActiveTranslation: boolean
+) => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   const tabId = tab.id;
-
-  const isChecked = translationElement.checked;
 
   if (!tabId) return;
 
   chrome.tabs.sendMessage(tabId, {
     message: "render",
-    isActiveTranslation: isChecked,
+    isActiveTranslation: isActiveTranslation,
   });
+};
+
+const getIsChecked = async (): Promise<boolean> => {
+  const { isChecked } = await chrome.storage.sync.get("isChecked");
+
+  return Boolean(isChecked);
+};
+
+const setIsChecked = async (isChecked: boolean) => {
+  await chrome.storage.sync.set({ isChecked });
+};
+
+const setSwitchState = async () => {
+  const isChecked = await getIsChecked();
+
+  translationElement.checked = isChecked;
+};
+
+translationElement.addEventListener("click", async () => {
+  const isChecked = translationElement.checked;
+
+  await setIsChecked(isChecked);
+  await sendToMessageIsActiveTranslation(isChecked);
 });
+
+setSwitchState();
