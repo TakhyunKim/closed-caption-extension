f44b25003843d66e3610fd5d57bb869d1d6c4b99
Takhyun Kim <kimkih1218@gmail.com>
2022-10-30 15:57:33 +0900

    feat: add isActiveTranslation field to render sendMessage API

--- src/popup.ts
+++ src/popup.ts
@@ -1,12 +1,19 @@
+import "./popup.css";
+
 const translationElement = document.getElementById(
   "translate"
-) as HTMLDivElement;
+) as HTMLInputElement;
 
 translationElement.addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   const tabId = tab.id;
 
+  const isChecked = translationElement.checked;
+
   if (!tabId) return;
 
-  chrome.tabs.sendMessage(tabId, { message: "render" });
+  chrome.tabs.sendMessage(tabId, {
+    message: "render",
+    isActiveTranslation: isChecked,
+  });
 });
