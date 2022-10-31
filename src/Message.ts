3ef82a7fe7f92866dded88735c4f424f10643aaf
Takhyun Kim <kimkih1218@gmail.com>
2022-10-31 23:36:35 +0900

    feat: add Message api handler

---
+++ src/Message.ts
@@ -0,0 +1,30 @@
+export type ChromeAPIRequest = {
+  message: string;
+  payload: string;
+};
+
+export type ChromeAPIResponse = {
+  error?: string;
+  data: string | null;
+};
+
+const Message = {
+  async sendMessageToContentScript(
+    tabId: number,
+    message: string,
+    data: unknown
+  ) {
+    chrome.tabs.sendMessage(tabId, { message, data });
+  },
+
+  async sendMessageToBackground(message: string, payload: string) {
+    const response = chrome.runtime.sendMessage<
+      ChromeAPIRequest,
+      ChromeAPIResponse
+    >({ message, payload });
+
+    return response;
+  },
+};
+
+export default Message;
