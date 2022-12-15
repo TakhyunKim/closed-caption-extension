b162ce96fcf78463894b15b425e3d914dc1456b7
Takhyun Kim <kimkih1218@gmail.com>
2022-12-16 00:59:33 +0900

    feat: apply FontSlider class and send message to content script

--- src/popup.ts
+++ src/popup.ts
@@ -3,11 +3,27 @@ import "./popup.css";
 import Storage from "./Storage";
 import Message from "./message";
 
-import { TRANSLATE_CALL_MESSAGE, SWITCH_STORAGE_KEY } from "./const";
+import FontSlider from "./popupView/FontSlider";
+
+import {
+  SWITCH_STORAGE_KEY,
+  TRANSLATE_CALL_MESSAGE,
+  FONT_SIZE_RANGE_MESSAGE,
+} from "./const";
 
 const translationElement = document.getElementById(
   "translate"
 ) as HTMLInputElement;
+
+const rangeElement = document.getElementById(
+  "font-size-range"
+) as HTMLInputElement;
+
+const rangeInputElement = document.querySelector(
+  'input[type="range"]'
+) as HTMLInputElement;
+
+const FontSliderInstance = new FontSlider(rangeElement);
 
 const sendToMessageIsActiveTranslation = async (
   isActiveTranslation: boolean
@@ -39,6 +55,21 @@ translationElement.addEventListener("click", async () => {
 
   await Storage.setStorageValue(SWITCH_STORAGE_KEY, isChecked);
   await sendToMessageIsActiveTranslation(isChecked);
+});
+
+rangeInputElement.addEventListener("input", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  const tabId = tab.id;
+
+  FontSliderInstance.setFontRangeStyle();
+
+  if (!tabId) return;
+
+  await Message.sendMessageToContentScript(
+    tabId,
+    FONT_SIZE_RANGE_MESSAGE,
+    Number(rangeElement.value)
+  );
 });
 
 // when popup open set default switch state
