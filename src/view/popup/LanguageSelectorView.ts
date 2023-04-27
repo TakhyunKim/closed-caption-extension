fb04c8498a996f48ab4013ba5cd81146d269e1ea
Takhyun Kim <kimkih1218@gmail.com>
2023-04-27 18:57:08 +0900

    feat: add LanguageSelectorView Class and test code

---
+++ src/view/popup/LanguageSelectorView.ts
@@ -0,0 +1,42 @@
+import type { LanguageCode } from "../../common/language.types";
+
+class LanguageSelectView {
+  private readonly languageSelector: HTMLDivElement;
+
+  constructor() {
+    this.languageSelector = document.getElementById(
+      "language-controller"
+    ) as HTMLDivElement;
+  }
+
+  private updateSelectedLanguageStyle = (selectedElement: SVGElement) => {
+    selectedElement.classList.toggle("hidden");
+    selectedElement.classList.toggle("selected");
+  };
+
+  public getLanguageSelector = () => {
+    return this.languageSelector;
+  };
+
+  public toggleLanguageSelectorDisplay = () => {
+    // toggle 이 되면 이거 체크
+    this.languageSelector.classList.toggle("hidden");
+  };
+
+  public toggleCheckLanguage = (language: LanguageCode) => {
+    const targetLanguageElement = document.querySelector(
+      `[data-lang="${language}"]`
+    ) as HTMLDivElement;
+    const targetLanguageSvgElement = targetLanguageElement
+      .children[1] as SVGElement;
+
+    this.updateSelectedLanguageStyle(targetLanguageSvgElement);
+  };
+
+  public updateLanguage = (language: LanguageCode) => {
+    this.toggleCheckLanguage(language);
+    this.toggleLanguageSelectorDisplay();
+  };
+}
+
+export default LanguageSelectView;
