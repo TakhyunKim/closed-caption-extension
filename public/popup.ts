eb1b53a35ab537d4a98a18b6051cefb979241102
Takhyun Kim <kimkih1218@gmail.com>
2023-05-28 00:11:55 +0900

    feat: update popup code

--- public/popup.ts
+++ public/popup.ts
@@ -1,4 +1,5 @@
 import FontSliderView from "../src/view/popup/FontSliderView";
+import TextColorPickerView from "../src/view/popup/TextColorPickerView";
 import LanguageSelectView from "../src/view/popup/LanguageSelectorView";
 import TranslatingSwitchView from "../src/view/popup/TranslatingSwitchView";
 import LanguageSelectorButtonView from "../src/view/popup/LanguageSelectorButtonView";
@@ -8,12 +9,14 @@ import PopupModel from "../src/model/popup/PopupModel";
 import PopupController from "../src/controller/popup/PopupController";
 
 import {
+  sendMessageToContentTextColor,
   sendMessageToContentRangeValue,
   sendMessageToContentChangedLanguage,
   sendMessageToContentIsActiveTranslation,
 } from "../src/api/message";
 
 const fontSliderView = new FontSliderView();
+const textColorPickerView = new TextColorPickerView();
 const languageSelectorView = new LanguageSelectView();
 const translatingSwitchView = new TranslatingSwitchView();
 const languageSelectorButtonView = new LanguageSelectorButtonView();
@@ -22,15 +25,18 @@ const popupModel = new PopupModel();
 
 const popupController = new PopupController(
   fontSliderView,
+  textColorPickerView,
   languageSelectorView,
   translatingSwitchView,
   languageSelectorButtonView,
   popupModel,
   sendMessageToContentIsActiveTranslation,
+  sendMessageToContentTextColor,
   sendMessageToContentRangeValue,
   sendMessageToContentChangedLanguage
 );
 
+popupController.setInitialTextColor();
 popupController.setInitialFontRangeSlider();
 popupController.setInitialSwitchState();
 popupController.setInitialLanguageSelectorAndButton();
