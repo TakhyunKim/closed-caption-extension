977ed557fbdebdc420636e23c0b3f9fbe125bc2c
Takhyun Kim <kimkih1218@gmail.com>
2023-05-28 00:11:42 +0900

    feat: apply text color picker view to Popup controller

--- src/controller/popup/PopupController.ts
+++ src/controller/popup/PopupController.ts
@@ -1,41 +1,50 @@
 import FontSliderView from "../../view/popup/FontSliderView";
+import TextColorPickerView from "../../view/popup/TextColorPickerView";
 import LanguageSelectorView from "../../view/popup/LanguageSelectorView";
 import TranslatingSwitchView from "../../view/popup/TranslatingSwitchView";
 import LanguageSelectorButtonView from "../../view/popup/LanguageSelectorButtonView";
 
 import PopupModel from "../../model/popup/PopupModel";
 
+import { throttle } from "../../common/throttle";
+
 import type { LanguageCode } from "../../common/language.types";
 
 class PopupController {
   fontSliderView: FontSliderView;
+  textColorPickerView: TextColorPickerView;
   languageSelectorView: LanguageSelectorView;
   translatingSwitchView: TranslatingSwitchView;
   languageSelectorButtonView: LanguageSelectorButtonView;
   popupModel: PopupModel;
 
   switchCallback: (isChecked: boolean) => Promise<void>;
+  textColorCallback: (textColor: string) => Promise<void>;
   fontSliderCallback: (fontSize: number) => Promise<void>;
   languageSelectorCallback: (languageCode: LanguageCode) => Promise<void>;
 
   constructor(
     _fontSliderView: FontSliderView,
+    _textColorPickerView: TextColorPickerView,
     _languageSelectorView: LanguageSelectorView,
     _translatingSwitchView: TranslatingSwitchView,
     _languageSelectorButtonView: LanguageSelectorButtonView,
     _popupModel: PopupModel,
 
     _switchCallback: (isChecked: boolean) => Promise<void>,
+    _textColorCallback: (textColor: string) => Promise<void>,
     _fontSliderCallback: (fontSize: number) => Promise<void>,
     _languageSelectorCallback: (languageCode: LanguageCode) => Promise<void>
   ) {
     this.fontSliderView = _fontSliderView;
+    this.textColorPickerView = _textColorPickerView;
     this.languageSelectorView = _languageSelectorView;
     this.translatingSwitchView = _translatingSwitchView;
     this.languageSelectorButtonView = _languageSelectorButtonView;
     this.popupModel = _popupModel;
 
     this.switchCallback = _switchCallback;
+    this.textColorCallback = _textColorCallback;
     this.fontSliderCallback = _fontSliderCallback;
     this.languageSelectorCallback = _languageSelectorCallback;
 
@@ -44,6 +53,14 @@ class PopupController {
       .addEventListener("input", () =>
         this.updateFontSlider(this.fontSliderCallback)
       );
+
+    this.textColorPickerView.getElement().addEventListener(
+      "input",
+      throttle(
+        (event) => this.updateTextColor(event, this.textColorCallback),
+        300
+      )
+    );
 
     this.translatingSwitchView
       .getElement()
@@ -75,6 +92,18 @@ class PopupController {
     await callback(rangeValue);
   };
 
+  private updateTextColor = async (
+    event: Event,
+    callback: (textColor: string) => Promise<void>
+  ) => {
+    const target = event.target as HTMLInputElement;
+    const textColor = target.value;
+
+    this.popupModel.setTextColor(textColor);
+
+    callback(textColor);
+  };
+
   private updateTranslate = async (
     callback: (isChecked: boolean) => Promise<void>
   ) => {
@@ -103,6 +132,13 @@ class PopupController {
     this.translatingSwitchView.setValue(isChecked);
 
     await this.switchCallback(isChecked);
+  };
+
+  public setInitialTextColor = async () => {
+    const textColor = await this.popupModel.getTextColor();
+    this.textColorPickerView.setValue(textColor);
+
+    await this.textColorCallback(textColor);
   };
 
   public setInitialFontRangeSlider = async () => {
